e8cec1279e3a3a1dcf856f79b0ac32214c48374d
isaiahwong <iisaiah.dev@gmail.com>
2025-03-31 21:51:13 +0800

    Added rust

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,7 @@
 -- Add any additional options here
 
 vim.g.lazyvim_python_lsp = "pyright"
+
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+
 vim.opt.conceallevel = 0
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,21 +1,32 @@
 return {
   {
     "xiantang/darcula-dark.nvim",
-    config = function()
-      require("darcula").setup({
-        override = function(c)
-          return {
-            --            dark = "#1C1D1F",
-          }
-        end,
-      })
-    end,
+    -- config = function()
+    --   -- setup must be called before loading
+    --   require("darcula").setup({
+    --     override = function(c)
+    --       return {
+    --         dark = "#1C1D1F",
+    --       }
+    --     end,
+    --     --   opt = {
+    --     --     integrations = {
+    --     --       lualine = true,
+    --     --       lsp_semantics_token = true,
+    --     --       nvim_cmp = true,
+    --     --       dap_nvim = true,
+    --     --     },
+    --     --   },
+    --   })
+    -- end,
   },
-  { "EdenEast/nightfox.nvim" },
+  { "Mofiqul/vscode.nvim" },
+  { "tanvirtin/monokai.nvim" },
+  { "navarasu/onedark.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "darcula-dark",
+      colorscheme = "vscode",
     },
   },
 }
